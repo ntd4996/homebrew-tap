4ceb2561342cd8d694765b2cbdfe73e4a3cb43bb
Dat Nguyen <ntd4996@gmail.com>
2026-06-02 23:11:45 +0700

    agentpet 1.1.0

--- Casks/agentpet.rb
+++ Casks/agentpet.rb
@@ -1,6 +1,6 @@
 cask "agentpet" do
-  version "1.0.2"
-  sha256 "41b17d5028af9b6be44a3c02ea33584bcca10fed409ac944e5149c9c8e7e86af"
+  version "1.1.0"
+  sha256 "28d3af475995f82b762d62f84115bb0f8e64b830cb6b195ba74f0c795bc3b771"
 
   url "https://github.com/ntd4996/agentpet/releases/download/v#{version}/AgentPet-#{version}.dmg"
   name "AgentPet"
