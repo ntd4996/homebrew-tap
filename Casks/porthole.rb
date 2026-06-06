76d6d8580ff62b675d3886cb965ce8dc41e0f06f
datnt <ntd4996@gmail.com>
2026-06-06 16:30:08 +0700

    porthole 1.0.2

--- Casks/porthole.rb
+++ Casks/porthole.rb
@@ -1,6 +1,6 @@
 cask "porthole" do
-  version "1.0.1"
-  sha256 "e1a535d440871ad798415d763b80814523a7ada3708f72f7419adcf6018bb95d"
+  version "1.0.2"
+  sha256 "2d6a93da2799a722d219c8594c6a9ba1ac2e2bfd49df43b41cec26127be972aa"
 
   url "https://github.com/ntd4996/Porthole/releases/download/v#{version}/Porthole-#{version}.dmg"
   name "Porthole"
