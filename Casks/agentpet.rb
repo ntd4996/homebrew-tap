fa4eb0dcb7fe8de3415834bf55624a0d4c6b84a4
datnt <ntd4996@gmail.com>
2026-08-18 11:55:21 +0700

    agentpet 1.14.3

--- Casks/agentpet.rb
+++ Casks/agentpet.rb
@@ -1,6 +1,6 @@
 cask "agentpet" do
-  version "1.14.2"
-  sha256 "d8176424121b1769b592cb7ca2247d2b2c2cdff648f9e177eff9cefc02ee29c1"
+  version "1.14.3"
+  sha256 "745a9f6195668e6a3399c38d22e296eac90ccf86f3f071a2d4c9bcdd01999cde"
 
   url "https://github.com/ntd4996/agentpet/releases/download/v#{version}/AgentPet-#{version}.dmg",
       verified: "github.com/ntd4996/agentpet/"
